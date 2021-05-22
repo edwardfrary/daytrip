691e17a2d5cf6eeea35a48536b5da8078d347c46
edwardfrary <edwardfrary@gmail.com>
2021-05-22 17:01:52 -0400

    event pulling ability

--- assets/javascript/jscript.js
+++ assets/javascript/jscript.js
@@ -1,22 +1,58 @@
 //API KEYS
-var eventAPI = "7J7BKJH5MNJRZSFHOQWE";
+var eventAPI = "NjCxcbmpRsWLzuFvW8Gr95HtW4UlTCbG";
 
-function apiSearch() {    
-    fetch("https://www.eventbrite.com/oauth/authorize?response_type=code&client_id="+ eventAPI)
+//TICKETMASTER SEARCH
+function eventFetch (){
+
+fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + eventAPI + "&city=new+york&radius=10")
         .then(function (response) {
             //check if there is a response then json the data and send it to be processed
             if (response.ok) {
                 response.json().then(function (data) {
-                    return data;
+                    console.log(data);
+                    eventsHandler(data);
                 });
             }
-
             //display error message if no response
             else {
                 alert("INVALID ENTRY");
-            }
+            };
         })
 };
 
-apiSearch();
-console.log(apiSearch());
+
+//splits the ticketmaster data into single events to be rendered
+function eventsHandler(data){
+    
+    for (i = 0;i<data._embedded.events.length;i++){
+        renderEvents(data._embedded.events[i]);
+    }
+};
+
+//renders the data into cards from ticketmaster
+function renderEvents(data){
+
+ //create the card container and then the other elements of the card
+ var cardCellEl = $("<div>")
+ .addClass("cell");
+var cardContainerEl = $("<div>")
+ .addClass("card");
+var cardHeaderEl = $("<div>")
+ .addClass("card-divider")
+ .text(data.name);
+var cardBodyDescEl = $("<div>")
+ .addClass("card-section")
+ .text(data.info);
+ var cardBodyLocEl = $("<div>")
+ .addClass("card-section")
+ .text(data._embedded.venues[0].name);
+
+$(cardContainerEl).append(cardHeaderEl);
+$(cardContainerEl).append(cardBodyDescEl);
+$(cardContainerEl).append(cardBodyLocEl);
+$(cardCellEl).append(cardContainerEl);
+$("#event-info").append(cardCellEl);
+
+}
+
+eventFetch();
